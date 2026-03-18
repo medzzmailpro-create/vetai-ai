ca63d38544b2192a0a7b44ac1006811569963d56
Medzz <medzz.mailpro@gmail.com>
2026-03-18 17:53:59 +0100

    aazeaez*ù.pagetsx

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,6 @@
+export const revalidate = 0
+export const dynamic = 'force-dynamic'
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
